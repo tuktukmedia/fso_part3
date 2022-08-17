cdbdb57fa381865d38fafcc60922d26ed342c31a
tuktukmedia <jani.uljas@tuktukmedia.com>
2022-08-17 13:47:39 +0300

    backend

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-const { request, response } = require("express")
-const express = require("express")
+const { request, response } = require('express')
+const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('postData', (request) => {
+  if (request.method == 'POST') return ' ' + JSON.stringify(request.body)
+  else return ' '
+})
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :postData'
+  )
+)
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
     id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
 ]
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find((person) => person.id === id)
 
@@ -42,13 +54,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 })
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   const date = new Date()
   const entries = persons.length
   response.send(`<p>Phonebook has info for ${entries} people</p><p>${date}</p>`)
 })
 
-app.post("/api/persons", (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
   const newID = Math.floor(Math.random() * 5000)
 
@@ -56,17 +68,17 @@ app.post("/api/persons", (request, response) => {
     /* virheen käsittelyt. tarkistetaan nimi, numero ja uniikki nimi*/
   }
   if (!body.name) {
-    return response.status(400).json({ error: "name missing" })
+    return response.status(400).json({ error: 'name missing' })
   }
   if (!body.number) {
-    return response.status(400).json({ error: "number missing" })
+    return response.status(400).json({ error: 'number missing' })
   }
   if (
     persons.find(
       (person) => person.name.toLowerCase() === body.name.toLowerCase()
     )
   ) {
-    return response.status(400).json({ error: "name must be unique" })
+    return response.status(400).json({ error: 'name must be unique' })
   }
 
   const person = {
@@ -79,12 +91,18 @@ app.post("/api/persons", (request, response) => {
   response.json(person)
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter((person) => person.id !== id)
 
   response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
