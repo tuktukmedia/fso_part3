2e91c5e227443873a958d1c94e1f4b05981be997
tuktukmedia <jani.uljas@tuktukmedia.com>
2022-08-17 14:43:37 +0300

    siirto

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { request, response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
